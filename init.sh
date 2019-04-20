1c78166d5422f0d99b9f99d7b7d40a6236f369af
leoluyi <leo0650@gmail.com>
2019-04-21 00:50:38 +0800

    Update

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 # export FLASK_APP="myapp.app:create_app()"
 # flask run
 
-pipenv run gunicorn -b localhost:5000 --access-logfile - "myapp.app:create_app()"
+pipenv run gunicorn -b 0.0.0.0:5000 --access-logfile - "myapp.app:create_app()"

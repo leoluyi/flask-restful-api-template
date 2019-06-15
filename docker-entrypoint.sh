a4c953035655e914cbb38f63f8516ddcb8579200
leoluyi <leo0650@gmail.com>
2019-06-15 12:02:25 +0800

    Add comment to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# For flask script
 export FLASK_APP="myapp.flasky"
 flask db init
 flask db migrate
 flask db upgrade
 
+# Run app
 gunicorn -c python:config.gunicorn "myapp.app:create_app()"
